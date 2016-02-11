7822d689ec990b3a53600642f51c97c246afe0c9
Kasper Nissen <kni@it-minds.dk>
2016-02-11 15:27:54 +0100

    Initial commit - generated spring boot project added and adapted the gitignore to handle intellij files as well

---
+++ src/main/java/com/rpicloud/SpringBootRestExampleApplication.java
@@ -0,0 +1,12 @@
+package com.rpicloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestExampleApplication.class, args);
+	}
+}
